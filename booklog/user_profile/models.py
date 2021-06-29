de6ad743c04b68986c93292bbbebaa1fb1f310a1
portfolio_1st <aa991122aa@gmail.com>
2021-06-29 21:52:41 +0900

    models.py 내용만 채움

--- booklog/user_profile/models.py
+++ booklog/user_profile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # # User모델과 Profile을 1:1로 연결
+    image = models.ImageField(blank=True)
+    nickname = models.CharField(max_length=40, blank=True)
+    description = models.TextField(blank=True)
