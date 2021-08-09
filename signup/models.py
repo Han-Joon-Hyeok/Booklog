1b45a6cda0fab719e872667f493baaf40594ebe0
fogus14 <fogus14@naver.com>
2021-08-09 19:39:42 +0900

    Add signup - signup(정래현)

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class MyUser(AbstractUser):
+    name = models.CharField(max_length=30)
