656eff107f4f37f4c06fe56d983c218f6f25605f
wille318 <wille318@naver.com>
2021-08-10 09:07:26 +0900

    follow part(view, models.py)추후 수정 필요 - 김예빈

--- follow/models.py
+++ follow/models.py
@@ -1,3 +1,27 @@
+# from django.db import models
+
+# # Create your models here.
+
+# ========= 기존에 코드가 재대로 실행이 안되었던 부분입니다.(기존작업물 업로드) ==========
+# python manage.py makemigrations & migrate 안한 상태
+
 from django.db import models
 
-# Create your models here.
+# 팔로우
+class Follow(models.Model):
+    follower = models.ForeignKey('MyUser', related_name="follower", on_delete=models.CASCADE)
+    following = models.ForeignKey('MyUser', related_name='follwing', on_delete=models.CASCADE)
+
+    class Mata:
+        db_table = 'follows'
+
+# 좋아요
+class Like(models.Model) :
+    like_username = models.ForeignKey('myuser.MyUser', on_delete = models.CASCADE)
+    #=== 좋아요 누르는 각각의 피드 입력해넣어야함. (app.Modes 이부분)====
+    posting = models.ForeignKey("app.Model", on_delete=models.CASCADE)
+    #===========================================================
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta :
+        db_table='likes'
