c1a755e0bcaafaa44c164dc4f35b4e4ecc8b8c43
portfolio_1st <aa991122aa@gmail.com>
2021-08-11 20:11:24 +0900

    signup test 완료

--- user/models.py
+++ user/models.py
@@ -1,11 +1,12 @@
+from signup.models import MyUser
 from django.db import models
-from django.conf import settings
-
+# from django.conf import settings
+from signup.models import MyUser
 # Create your models here.
 
 class Profile(models.Model):
-    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
     # # User모델과 Profile을 1:1로 연결
-    image = models.ImageField(blank=True)
-    nickname = models.CharField(max_length=40, blank=True)
-    description = models.TextField(blank=True)
+    image = models.ImageField(blank=True,  default='')
+    nickname = models.CharField(max_length=40, blank=True,  default='')
+    description = models.TextField(blank=True, default='')
--- user/views.py
+++ user/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import redirect, render
 from .models import Profile
+from signup.models import MyUser
 from .forms import ProfileForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
+def writeprofile(request, id):
+    User = get_user_model()
+    user = get_object_or_404(User, id=id)
+    profile = Profile.objects.get(id=id)
+    context = {
+        'user': user,
+        'profile':profile
+    }
+    return render(request, 'writeprofile.html', context)
 
 
 # def writeprofile(request, id):
@@ -12,17 +24,23 @@ def writeprofile(request):
         form = ProfileForm(request.POST,request.FILES)
         if form.is_valid():
             # clean data
-            profile = Profile()
-            # profile = Profile.objects.get(id=id)
+            # profile = Profile()
+            profile = Profile.objects.get(id=id)
             profile.image = request.POST['image']
             profile.nickname = request.POST['nickname']
             profile.description = request.POST['description']
             profile.save()
-            print()
             return redirect('profile')
-    # else:
+            # return render(request,'profile',id=id)
+    else:
         # login 쪽에서 이메일이랑 아이디 가져와야함.
-    return render(request, 'writeprofile.html' )
+        profile = Profile.objects.get(id=id)
+        myuser = MyUser.objects.get(id=id)
+        context ={
+            'profile':profile,
+            'myuser':myuser,
+        }
+        return render(request, 'writeprofile.html',context)
 
 
 
