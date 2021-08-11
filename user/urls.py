3fbfb8cb60ddee0381d81431d09d84767a83f1bb
Siyoung <happycloudy0308@gmail.com>
2021-08-11 21:48:05 +0900

    프로필 이름 반영

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('profile/', views.profile, name="profile"),
     path('writeProfile/', views.writeprofile, name="writeprofile"),
     # path('writeProfile/<str:id>', pv.writeprofile, name="writeprofile"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Profile
 from .forms import ProfileForm
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
 
-
 # def writeprofile(request, id):
 def writeprofile(request):
-    if request.method =='POST':
-        form = ProfileForm(request.POST,request.FILES)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
             # clean data
             profile = Profile()
@@ -22,11 +22,13 @@ def writeprofile(request):
             return redirect('profile')
     # else:
         # login 쪽에서 이메일이랑 아이디 가져와야함.
-    return render(request, 'writeprofile.html' )
-
+    return render(request, 'writeprofile.html')
 
 
 def profile(request):
-    return render(request, "profile.html")
-
-
+    User = get_user_model()
+    user = get_object_or_404(User)
+    context = {
+        'user': user
+    }
+    return render(request, "profile.html", context)
