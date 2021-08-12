283ee7771a631b30dc9dbe4361cc337642d99091
portfolio_1st <aa991122aa@gmail.com>
2021-08-13 02:33:43 +0900

    views.py 프린트 제거및 writeprofile 사진 업로드 해결

--- user/views.py
+++ user/views.py
@@ -21,10 +21,6 @@ def writeprofile(request, id):
             profile.image=request.FILES['image']
             profile.nickname = request.POST['nickname']
             profile.description = request.POST['description']
-            print('저장하기')
-            print('사진 : ',profile.image)
-            print('이름 : ',profile.nickname)
-            print('설명 : ',profile.description)
             profile.save()
             form.save()
             return redirect('profile',user.id)
@@ -34,30 +30,15 @@ def writeprofile(request, id):
             'user':user,
             'profile':profile,
         }   
-        print('불러오기')
-        print('사진 : ',profile.image)
-        print('이름 : ',profile.nickname)
-        print('설명 : ',profile.description)
         return render(request, "writeProfile.html", context)
 
 
 def profile(request):
     profile = Profile.objects.get(user=MyUser.objects.get(name=request.user.name))
-    print(1)
     cnt = Post.objects.filter(profile=profile).count()
-    print(2)
     response_data = {}
     response_data["count"] = cnt
-    print(4)
     response_data["profile"] = profile
-    print(5)
-    print(cnt)
-    print()    
-    print()    
-    print()    
-    print()    
-    print()    
-
     # <test용
     # print(user.id)
     # print(user.username)
