b1ce850d23242a53e8651543cfb9fe870b92b21f
joonhyuk.han <virq@naver.com>
2021-08-13 20:27:44 +0900

    fix : settings.py 배포 환경설정 수정'

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!#(yb1o*59+4+7fj$e)*)@o9409^)*(r=gedv(4jbinuzfnkgi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'joon.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -115,12 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'base/static'),
-    os.path.join(BASE_DIR, 'user/static'),
-
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'base/static'),
+#     os.path.join(BASE_DIR, 'user/static'),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 이용자가 업로드한 파일을 모으는 곳
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
